e7234f3fdbd701eb6290bb1186afe2867be4c294
Ellen Leino <ellen.leino@tuni.fi>
2025-03-12 17:24:47 +0200

    resizing ja muuta pikkusäätöö

--- Scripts/GameManager.cs
+++ Scripts/GameManager.cs
@@ -82,10 +82,6 @@ namespace escapetampere
 
 		}
 
-		private void OnSettingsButtonPressed()
-		{
-		}
-
 		// Called every frame. 'delta' is the elapsed time since the previous frame.
 		public override void _Process(double delta)
 		{
