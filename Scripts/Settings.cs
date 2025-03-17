85a9433230306fa27ac0ddc76a4ca975a085c408
NiinaTarvaala <niina.tarvaala@tuni.fi>
2025-03-17 08:26:28 +0200

    Äänet ja html muokkauksia

--- Scripts/Settings.cs
+++ Scripts/Settings.cs
@@ -2,16 +2,49 @@ using Godot;
 
 namespace escapetampere
 {
-public partial class Settings : Control
-{
-	    public void Open()
+    public partial class Settings : Control
     {
-        Visible = true; // Show the panel
-    }
+        private AudioStreamPlayer _audioPlayer;
+        private Button _soundOnButton;
+        private Button _soundOffButton;
+        private bool _isSoundOn = true;
 
-    public void Close()
-    {
-        Visible = false; // Hide the panel
+        public override void _Ready()
+        {
+            // Hae AudioStreamPlayer ja Button-solmut
+            _audioPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+            _soundOnButton = GetNode<Button>("SoundOnButton");
+            _soundOffButton = GetNode<Button>("SoundOffButton");
+
+            // Yhdistä signaalit nappeihin
+            _soundOnButton.Pressed += OnSoundOnButtonPressed;
+            _soundOffButton.Pressed += OnSoundOffButtonPressed;
+
+            // Aseta napit näkyviksi tai piilota niitä äänen tilan mukaan
+            UpdateButtonVisibility();
+        }
+
+        // SoundOnButtonin painallus
+        private void OnSoundOnButtonPressed()
+        {
+            _isSoundOn = true;
+            _audioPlayer.Play();
+            UpdateButtonVisibility();
+        }
+
+        // SoundOffButtonin painallus
+        private void OnSoundOffButtonPressed()
+        {
+            _isSoundOn = false;
+            _audioPlayer.Stop();
+            UpdateButtonVisibility();
+        }
+
+        // Päivitä nappien näkyvyys äänen tilan mukaan
+        private void UpdateButtonVisibility()
+        {
+            _soundOnButton.Visible = !_isSoundOn;
+            _soundOffButton.Visible = _isSoundOn;
+        }
     }
-}
 }
