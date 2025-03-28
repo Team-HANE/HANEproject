3b95edf4c25cbb2de016bb48b00295c988700213
Ellen Leino <ellen.leino@tuni.fi>
2025-03-28 12:57:12 +0200

    nappipäivitys

--- Scripts/LanguageData.cs
+++ Scripts/LanguageData.cs
@@ -4,7 +4,7 @@ using System;
 [GlobalClass]
 public partial class LanguageData : Resource
 {
-    [Export] public string CurrentLocale = "fi";
+    [Export] public string CurrentLocale = "en";
 
     public void SetLanguage(string locale)
     {
--- Scripts/LanguageSwitch.cs
+++ Scripts/LanguageSwitch.cs
@@ -5,14 +5,43 @@ public partial class LanguageSwitch : CanvasLayer
 {
 	private LanguageData _languageData;
 
+	[Export] private Texture2D fiActive;
+	[Export] private Texture2D fiInactive;
+	[Export] private Texture2D engActive;
+	[Export] private Texture2D engInactive;
+
+	private Button fi;
+	private Button en;
+
 	public override void _Ready()
 	{
 		_languageData = ResourceLoader.Load<LanguageData>("res://LanguageData.tres");
-		GetNode<Button>("Fi").Pressed += () => ChangeLanguage("fi");
-		GetNode<Button>("Eng").Pressed += () => ChangeLanguage("en");
+		fi = GetNode<Button>("Fi");
+		en = GetNode<Button>("Eng");
+		fi.Pressed += () => ChangeLanguage("fi");
+		en.Pressed += () => ChangeLanguage("en");
+
+		UpdateButtonIcons (_languageData.CurrentLocale);
 	}
 	private void ChangeLanguage(string locale)
 	{
 		_languageData.SetLanguage(locale);
+		UpdateButtonIcons(locale);
+	}
+
+	private void UpdateButtonIcons(string activeLocale)
+	{
+		// When Finnish is active, show its active texture and the inactive texture for English
+		if (activeLocale == "fi")
+		{
+			fi.Icon = fiActive;
+			en.Icon = engInactive;
+		}
+		// When English is active, show its active texture and the inactive texture for Finnish
+		else if (activeLocale == "en")
+		{
+			fi.Icon = fiInactive;
+			en.Icon = engActive;
+		}
 	}
 }
